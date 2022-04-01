5623b01c1b18332e784d8ca0a227c007dd49ffe5
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-04-01 09:28:31 -0500

    moved output

--- src/main/java/PizzaServlet.java
+++ src/main/java/PizzaServlet.java
@@ -14,20 +14,23 @@ public class PizzaServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String car = request.getParameter("crust");
-        response.getWriter().println(car);
-
+        String crust = request.getParameter("crust");
+//        response.getWriter().println(crust);
         String sauce = request.getParameter("sauce");
-        response.getWriter().println(sauce);
-
+//        response.getWriter().println(sauce);
         String size = request.getParameter("size");
-        response.getWriter().println(size);
-
+//        response.getWriter().println(size);
         String[] toppings = request.getParameterValues("toppings");
-        response.getWriter().println(Arrays.toString(toppings));
-
+//        response.getWriter().println(Arrays.toString(toppings));
         String address = request.getParameter("address");
-        response.getWriter().println(address);
+//        response.getWriter().println(address);
+
+        request.setAttribute("crust", crust);
+        request.setAttribute("sauce", sauce);
+        request.setAttribute("size", size);
+        request.setAttribute("toppings", Arrays.toString(toppings));
+        request.setAttribute("address", address);
+        request.getRequestDispatcher("/pizza-order-form.jsp").forward(request, response);
 
     }
 }
