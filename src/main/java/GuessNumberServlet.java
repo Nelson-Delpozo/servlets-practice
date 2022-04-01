7e4b2ebd2dcf45b6e678160d2f7b5ee12ae8305c
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-04-01 16:37:03 -0500

    unknown changes

--- src/main/java/GuessNumberServlet.java
+++ src/main/java/GuessNumberServlet.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class GuessNumberServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.getRequestDispatcher("/guess.jsp").forward(request, response);
     }
 
     @Override
