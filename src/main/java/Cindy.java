ec8418aa03ec3573a235126465155d871ddcfd1e
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-04-02 11:52:40 -0500

    cindy

---
+++ src/main/java/Cindy.java
@@ -0,0 +1,18 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "CindyServlet", value = "/CindyServlet")
+public class Cindy extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/cindy.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    }
+}
