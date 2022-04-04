7084954069d85d92f23a0b407a033ca748f88824
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-04-04 09:52:16 -0500

    added beans

---
+++ src/main/java/Album.java
@@ -0,0 +1,59 @@
+import java.io.Serializable;
+import java.util.Date;
+
+public class Album implements Serializable {
+    private long id;
+    private String artist;
+    private String name;
+    private Date releaseDate;
+    private String genre;
+    private float sales;
+
+    public Album(){};
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {this.releaseDate = releaseDate;}
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public float getSales() {
+        return sales;
+    }
+
+    public void setSales(float sales) {
+        this.sales = sales;
+    }
+}
