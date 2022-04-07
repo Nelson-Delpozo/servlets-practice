03616569560af1fc087a24b4bb7f5208a07a8b87
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-04-07 10:26:40 -0500

    changes

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -1,10 +1,25 @@
+//public class DaoFactory {
+//    private static Ads adsDao;
+//
+//    public static Ads getAdsDao() {
+//        if (adsDao == null) {
+//            adsDao = new ListAdsDao();
+//        }
+//        return adsDao;
+//    }
+//}
+
+import java.sql.SQLException;
+
 public class DaoFactory {
     private static Ads adsDao;
+    private static final Config config = new Config();
 
-    public static Ads getAdsDao() {
+    public static Ads getAdsDao() throws SQLException {
         if (adsDao == null) {
-            adsDao = new ListAdsDao();
+            adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
     }
 }
+
--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,44 +1,81 @@
-//import java.sql.*;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class MySQLAdsDao implements Ads {
-//
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.DriverManager;
+import com.mysql.cj.jdbc.Driver;
+
+public class MySQLAdsDao implements Ads {
+    private final Connection connection;
 //    private List<Ad> ads;
-//    private Connection connection;
-//
-//    public MySQLAdsDao() throws SQLException {
-////        Config config = new Config();
-////        Connection connection = DriverManager.getConnection(
-////                config.getUrl(),
-////                config.getUser(),
-////                config.getPassword());
-////        this.connection = connection;
-//    }
-//
-//    public List<Ad> all() throws SQLException {
-//        if (ads == null) {
-//            ads = generateAds();
-//        }
-//        return ads;
-//    }
-//
-//    public Long insert(Ad ad) throws SQLException {
-//        // make sure we have ads
-//        if (ads == null) {
-//            ads = generateAds();
-//        }
-//        // we'll assign an "id" here based on the size of the ads list
-//        // really the database would handle this
-//        ad.setId((long) ads.size());
-//        ads.add(ad);
-//        return ad.getId();
-//    }
-//
+
+    public MySQLAdsDao(Config config) throws SQLException {
+        DriverManager.registerDriver(new Driver());
+        connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUser(),
+                config.getPassword());
+
+    }
+
+
+    @Override
+    public List<Ad> all() throws SQLException {
+        Statement stmt;
+
+        stmt = connection.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
+        return createAdsFromResults(rs);
+
+    }
+
+    private List<Ad> createAdsFromResults(ResultSet rs) throws SQLException {
+
+        List<Ad> ads = new ArrayList<>();
+
+        while (rs.next()){
+            ads.add(extractAd(rs));
+
+        }
+        return ads;
+    }
+
+    private Ad extractAd(ResultSet rs) throws SQLException {
+        return new Ad(
+                rs.getLong("id"),
+                rs.getLong("user_id"),
+                rs.getString("title"),
+                rs.getString("description")
+        );
+    }
+
+    @Override
+    public Long insert(Ad ad) throws SQLException {
+
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+        ResultSet rs = statement.getGeneratedKeys();
+        rs.next();
+        return rs.getLong(1);
+    }
+
+    private String createInsertQuery(Ad ad) {
+        return "INSERT INTO ads(user_id, title, description) VALUES "
+                + "(" + ad.getUserId() + ", " //(id,'title','description')
+                + "'" + ad.getTitle() + "',"
+                + "'" + ad.getDescription() + "')";
+    }
+
 //    private List<Ad> generateAds() throws SQLException {
 //        List<Ad> ads = new ArrayList<>();
+//        String selectQuery = "SELECT * FROM ads";
+//        Statement stmt = connection.createStatement();
+//        ResultSet rs = stmt.executeQuery(selectQuery);
 //
+//        while (rs.next()) {
+//            System.out.println("rs.getString(\"title\") = " + rs.getString("title"));
 //
-//        return ads;
+//        }
+//        return null;
 //    }
-//}
+
+}
