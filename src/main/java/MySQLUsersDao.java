66d0fd7684a032ed1263b0514707552a957f0043
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-04-08 11:21:56 -0500

    added users dao

---
+++ src/main/java/MySQLUsersDao.java
@@ -0,0 +1,68 @@
+import com.mysql.cj.jdbc.Driver;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLUsersDao implements Users {
+    private Connection connection = null;
+
+    public MySQLUsersDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+    @Override
+    public Long insert(User user) {
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(createInsertQuery(user), Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating a new user.", e);
+        }
+    }
+
+    private String createInsertQuery(User user) {
+        return "INSERT INTO users(id, username, email, password) VALUES "
+                + "(" + user.getId() + ", "
+                + "'" + user.getUsername() +"', "
+                + "'" + user.getEmail() +"', "
+                + "'" + user.getPassword() + "')";
+    }
+
+    private User extractUser(ResultSet rs) throws SQLException {
+        return new User(
+                rs.getLong("id"),
+                rs.getString("username"),
+                rs.getString("email"),
+                rs.getString("password")
+        );
+    }
+
+    private List<User> createUsersFromResults(ResultSet rs) throws SQLException {
+        List<User> users = new ArrayList<>();
+        while (rs.next()) {
+            users.add(extractUser(rs));
+        }
+        return users;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
+
+}
