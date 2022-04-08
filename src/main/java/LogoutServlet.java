79417e3a63cc7db9d54438ddb9bba1ef129df194
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-04-08 12:26:27 -0500

    fixed logout button

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().removeAttribute("user");
         request.getSession().setAttribute("isLoggedIn", false);
         response.sendRedirect("/login");
     }
